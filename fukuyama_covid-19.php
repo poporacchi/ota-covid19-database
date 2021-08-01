23fc7c1c9c0c0fcd05dec12cad5976fcd823c956
poporacchi <kota.np@gmail.com>
2021-08-01 21:12:50 +0900

    push

--- fukuyama_covid-19.php
+++ fukuyama_covid-19.php
@@ -282,9 +282,9 @@ if(empty($str_last_updated_hiroshima)){
 echo "10万人あたり" . sprintf('%.1f',$cnt_total_hiroshima/28.1) . "人, 先週比：" . (int)(($cnt_total_hiroshima / $cnt_total2_hiroshima) * 100) . "%</h3>";
 
 if ($cnt_total_onomichi != 0) {
-  echo "尾道市陽性者数：" . $cnt_total_onomichi . "人/週（" . $cnt_total_onomichi/1.38 . "/10万人）<br />";
+  echo "尾道市陽性者数：" . $cnt_total_onomichi . "人/週（" . sprintf('%.1f', $cnt_total_onomichi/1.38) . "/10万人）<br />";
 } else if ($cnt_total_mihara != 0) {
-  echo "三原市陽性者数：" . $cnt_total_mihara . "人/週（" . $cnt_total_mihara/0.96 . "/10万人）<br />";
+  echo "三原市陽性者数：" . $cnt_total_mihara . "人/週（" . sprintf('%.1f', $cnt_total_mihara/0.96) . "/10万人）<br />";
 }
 
 if(empty($str_last_updated_hiroshima)){
